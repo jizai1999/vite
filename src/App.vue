987712fef072b61ea1bf2d2ec70a472dbc50cb9b
jizhai1999 <1260528165@qq.com>
2023-02-21 16:49:41 +0800

    额

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <button>22222</button>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6rem;
+  padding: 1.5rem;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2rem #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2rem #42b883aa);
+}
+</style>
